4aef389216e93179128d3a88c1e10217ddabe451
flfroehl <flfroehl@htwg-konstanz.de>
2014-04-16 16:33:22 +0200

    first Test to open and load.xmind files

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,102 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	
+	$('h1').click(function() {
+		addWarning("h1")
+	});
+	
+	
+	var ADD_AFTER_NODE_ID = "result";
+	var XML_PATH_ID = "xmlContentFilePath";
+	var XMIND_ENDING = ".xml";
+	var XML_CONTENT = "";
+	
+	function addWarning(it) {
+		alert(it);
+		$(it).addClass('warn');
+	}
 
-//$('#nurdas').attr({title: 'jQuery-Kurs', href: 'http://www.html-seminar.de/jquery.htm'});
-//$('#nurdas').text("der ist neu");
-//alert($('#nurdas').html());
-$('h1').click(function(){addWarning("h1")});
+    //on change of id="xmlContentFilePath"
+	xmlContentFilePath.onchange = function(e) {
+		var file = xmlContentFilePath.files[0];
+		var path = xmlContentFilePath.value;
 
-function addWarning(it)
-{
-	alert(it);
-	//$('h1').addClass('warn');
-	$(it).addClass('warn');
-}
+		if (path.contains(XMIND_ENDING)) {
+			
+			var reader = new FileReader();
+			reader.onload = function(e){
+          		XML_CONTENT = loadXMLString(e.target.result);
+          		alert(XML_CONTENT);
+			
+          		writeXML(XML_CONTENT, "sheet", document.getElementById(ADD_AFTER_NODE_ID));
+          		
+        	};
+			reader.readAsText(file);
+			
+		} else {
+			alert("Wrong Format! (should be "+XMIND_ENDING+")");
+		}
 
+	}
+	//andere Funktionen wie hier bearbeiten! wegen neuen Browsern...  Leerzeilen == empty textnode
 
-xmlfilePath.onchange = function(e) {
-alert(e.value);
-xmlSource=xmlfilePath.value;
+	String.prototype.contains = function(it) {
+		return this.indexOf(it) > 0;
+	};
 
+	function getFirstChild(n) {
+		y = n.firstChild;
+		while (y.nodeType != 1) {
+			y = y.nextSibling;
+		}
+		return y;
+	}
 
+	function loadXMLDoc(filename) {
 
-if(xmlSource.contains(".xmind"))
-{
-	alert("read stuff!");
-	var xmlDoc=loadXMLDoc(xmlSource);
-	alert("write stuff!");
-	writeXML(xmlDoc,"title");
-	alert("done!");
-}
-else{
-alert("no path");
-}
+		if (window.XMLHttpRequest) {
+			xhttp = new XMLHttpRequest();
+		} else// code for IE5 and IE6
+		{
+			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhttp.open("GET", filename, false);
+		xhttp.send();
+		return xhttp.responseXML;
+	}
 
+	function loadXMLString(txt) {
+		var xmlDoc;
+		if (window.DOMParser) {
+			parser = new DOMParser();
+			xmlDoc = parser.parseFromString(txt, "text/xml");
+		} else// code for IE
+		{
+			xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+			xmlDoc.async = false;
+			xmlDoc.loadXML(txt);
+		}
+		return xmlDoc;
+	}
 
-}
-//andere Funktionen wie hier bearbeiten! wegen neuen Browsern...  Leerzeilen == empty textnode
+	function writeXML(xml, elementName, addAfterNode) {
 
-String.prototype.contains = function(it) { return this.indexOf(it)  > 0; };
+		x = xml.getElementsByTagName(elementName);
+		$(addAfterNode).after(x);
+		alert("TODO change writeXML");
+		
+		/*for ( i = 0; i < x.length; i++) {
+			var n = x[i];
+			
+			document.write(n.childNodes[1].childNodes[0].nodeValue + "<br>");
 
-function getFirstChild(n)
-{
-y=n.firstChild;
-while (y.nodeType!=1)
-  {
-  y=y.nextSibling;
-  }
-return y;
-}
-
-function loadXMLDoc(filename)
-{
-
-if (window.XMLHttpRequest)
-  {
-  xhttp=new XMLHttpRequest();
-  }
-else // code for IE5 and IE6
-  {
-  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xhttp.open("GET",filename,false);
-xhttp.send();
-return xhttp.responseXML;
-}
-
-function loadXMLString(txt) 
-{
-if (window.DOMParser)
-  {
-  parser=new DOMParser();
-  xmlDoc=parser.parseFromString(txt,"text/xml");
-  }
-else // code for IE
-  {
-  xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-  xmlDoc.async=false;
-  xmlDoc.loadXML(txt); 
-  }
-return xmlDoc;
-}
-function writeXML(xml,elementName)
-{
-
-x=xml.getElementsByTagName(elementName);
-for (i=0;i<x.length;i++)
-  { 
-    var n=x[i];
-    document.write(n.childNodes[1].childNodes[0].nodeValue + "<br>");
-         
-    for (j=1;j<n.childNodes.length;j++)
-    {
-       if (x[i].childNodes[j].nodeType==1)
-       {
-          document.write(" -- "+ n.childNodes[j].nodeName +" --> "+    n.childNodes[j].childNodes[0].nodeValue);
-          document.write("<br>");
-       }
-    }
-  }
-}
+			for ( j = 1; j < n.childNodes.length; j++) {
+				if (x[i].childNodes[j].nodeType == 1) {
+					document.write(" -- " + n.childNodes[j].nodeName + " --> " + n.childNodes[j].childNodes[0].nodeValue);
+					document.write("<br>");
+				}
+			}
+			
+		}*/
+	}
 
 });
